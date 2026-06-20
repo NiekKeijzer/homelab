8d6a4fa59447641472aee04b975da4a643fb1164
Niek Keijzer <hello@niekkeijzer.com>
2026-06-20 20:32:20 +0200

    feat: add komodo  (#1)
    * feat: basic komodo setup
    
    * feat: bootstrap komodo syncs
    
    * feat: add whoami deployment for testing
    
    * chore(docs): write bootstrap instructions using debug mesage
    
    * feat: provision komodo nodes
    
    * fix: remove debug statement

--- infra/docker.tf
+++ infra/docker.tf
@@ -22,3 +22,43 @@ module "docker_vm" {
 
   ansible_groups = ["docker", ]
 } 
+
+module "periphery_vm" {
+ source = "./modules/proxmox_vm"
+
+  name_prefix = "periphery"
+  vm_count = local.docker_vm_count
+  tags = concat(["komodo", "periphery", "docker"], local.vm_template.tags)
+
+  cores = 4
+  memory_mb =  16384
+  disk_size_gb = 32
+
+  proxmox_node = var.proxmox_node
+  template_vm_id = local.vm_template.id
+
+  provision_user = local.provision.user
+  provision_ssh_public_keys = local.provision.public_keys
+
+  ansible_groups = ["periphery", "docker"] 
+}
+
+module "komodo_vm" {
+ source = "./modules/proxmox_vm"
+
+  name_prefix = "komodo"
+  vm_count = local.docker_vm_count
+  tags = concat(["komodo", "docker"], local.vm_template.tags)
+
+  cores = 2
+  memory_mb =  2048
+  disk_size_gb = 16
+
+  proxmox_node = var.proxmox_node
+  template_vm_id = local.vm_template.id
+
+  provision_user = local.provision.user
+  provision_ssh_public_keys = local.provision.public_keys
+
+  ansible_groups = ["komodo", "docker"] 
+}
